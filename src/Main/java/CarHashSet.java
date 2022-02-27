79681e9fd0ca9ea4077546f375fd1d7eaf78466f
lokalizator12 <83495533+lokalizator12@users.noreply.github.com>
2022-02-27 22:12:14 +0100

    1

---
+++ src/Main/java/CarHashSet.java
@@ -0,0 +1,104 @@
+public class CarHashSet implements CarSet {
+
+    public int size = 0;
+    private static final int INITIAL_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private Entry[] array = new Entry[INITIAL_CAPACITY];
+
+    @Override
+    public boolean add(Car car) {
+        if (size >= (array.length * LOAD_FACTOR)){
+            increaseArray();
+        }
+        boolean result = add(car, array);
+        if (result){
+            size++;
+        }
+    return result;
+    }
+
+
+    public boolean add(Car car, Entry[] dts){
+        int position = getIndexElement(car, dts.length);
+        if (dts[position] == null) {
+            Entry entry = new Entry(car, null);
+            dts[position] = entry;
+            return true;
+        }else{
+            Entry sameElement = dts[position];
+            while (true){
+                if (sameElement.value.equals(car)){
+                    return false;
+                }else if(sameElement.next == null){
+                    sameElement.next = new Entry(car,null);
+                    return true;
+                }else{
+                    sameElement = sameElement.next;
+                }
+            }
+        }
+    }
+
+    private void increaseArray(){
+        Entry[] secondArray = new Entry[array.length * 2];
+        for (Entry entry : array){
+            Entry existedElement = entry;
+            while (existedElement != null){
+                add(existedElement.value,secondArray);
+                existedElement = existedElement.next;
+            }
+        }
+    array = secondArray;
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        int position = getIndexElement(car, array.length);
+        if (array[position] == null) {
+            return false;
+        }
+        Entry secondLast = array[position];
+        Entry last = secondLast.next;
+        if(secondLast.value.equals(car)){
+            array[position] = last;
+            size--;
+            return true;
+        }
+       while (last != null){
+           if(last.value.equals(car)){
+               secondLast.next = last.next;
+               size--;
+               return true;
+           }else{
+               secondLast = last;
+               last = last.next;
+           }
+       }
+       return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    array = new Entry[INITIAL_CAPACITY];
+    }
+
+    public int getIndexElement(Car car, int arrayLength) {
+        return Math.abs(car.hashCode() % arrayLength);
+    }
+
+    private static class Entry {
+        private Car value;
+        private Entry next;
+
+        public Entry(Car value, Entry next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/CarSetTest.java
@@ -0,0 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarSetTest {
+    private CarSet car1;
+
+    @Before
+    public void setUp() throws Exception {
+        car1 = new CarHashSet();
+        for (int i = 0; i < 100; i++) {
+            car1.add(new Car("Brand" + i, i));
+        }
+    }
+
+    @Test
+    public void whenAddTwoSameAndOneOtherItemsThenResultIsSizeIsThe2() {
+        assertEquals(100, car1.size());
+        assertTrue(car1.add(new Car("BMW",8)));
+        assertFalse(car1.add(new Car("BMW",8)));
+        assertEquals(101, car1.size());
+    }
+
+    @Test
+    public void whenRemoveOneElementThenSizeResultIsMinusOne() {
+        assertTrue(car1.add(new Car("BMW", 8)));
+        assertFalse(car1.add(new Car("BMW", 8)));
+        assertTrue(car1.remove(new Car("BMW", 8)));
+        assertEquals(100, car1.size());
+
+    }
+
+    @Test
+    public void size() {
+        assertTrue(car1.add(new Car("BMW", 8)));
+        assertFalse(car1.add(new Car("BMW", 8)));
+        assertEquals(101, car1.size());
+    }
+
+    @Test
+    public void clear() {
+        car1.clear();
+        assertEquals(0, car1.size());
+    }
+}
